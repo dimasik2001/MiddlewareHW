b8878a179a2a3a1089a7229a348e98b549a87a8d
Dimasik2001 <dimonkalibaba@gmail.com>
2021-11-03 18:15:49 +0200

    HW

---
+++ MiddlewareHomework/ExtraHeaderAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareHomework
+{
+    public class ExtraHeaderAttribute: Attribute
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
